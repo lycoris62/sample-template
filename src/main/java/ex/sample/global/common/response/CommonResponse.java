542e91992d72143bb43d13eba8626f8e8658c32f
barami62 <wodbs7758@naver.com>
2024-09-05 01:47:16 +0900

    enhance: 공통 응답 내부 구현 수정

--- src/main/java/ex/sample/global/common/response/CommonResponse.java
+++ src/main/java/ex/sample/global/common/response/CommonResponse.java
@@ -16,10 +16,8 @@ public class CommonResponse<T> implements Serializable {
 
     @Schema(description = "응답 코드", example = "0")
     private Integer code; // 커스텀 응답 코드
-
     @Schema(description = "응답 메시지", example = "정상 처리 되었습니다.")
     private String message; // 응답에 대한 설명
-
     @Schema(description = "응답 데이터", example = "{}")
     private T data; // 응답에 필요한 데이터
 
@@ -27,20 +25,20 @@ public class CommonResponse<T> implements Serializable {
      * data 필드에 값을 넣을 때 사용하는 메서드 - data 필드가 필요 없는 경우
      */
     public static CommonResponse<CommonEmptyRes> success() {
-        return CommonResponse.<CommonEmptyRes>builder()
-            .code(ResultCase.SUCCESS.getCode())
-            .message(ResultCase.SUCCESS.getMessage())
-            .data(new CommonEmptyRes())
-            .build();
+        return getSuccessRes(new CommonEmptyRes());
     }
 
     /**
      * data 필드에 값을 넣을 때 사용하는 메서드 - data 필드가 필요한 경우
      */
     public static <T> CommonResponse<T> success(T data) {
+        return getSuccessRes(data);
+    }
+
+    private static <T> CommonResponse<T> getSuccessRes(T data) {
         return CommonResponse.<T>builder()
-            .code(ResultCase.SUCCESS.getCode())
-            .message(ResultCase.SUCCESS.getMessage())
+            .code(0)
+            .message("정상 처리 되었습니다.")
             .data(data)
             .build();
     }
