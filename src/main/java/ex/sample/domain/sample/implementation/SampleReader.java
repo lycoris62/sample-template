692b478648b89cc84bf62be51f3066fea98637d2
yunjae62 <wodbs7758@naver.com>
2024-09-24 04:01:42 +0900

    feat: 트랜잭션 추가

--- src/main/java/ex/sample/domain/sample/implementation/SampleReader.java
+++ src/main/java/ex/sample/domain/sample/implementation/SampleReader.java
@@ -10,10 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class SampleReader {
 
     private final SampleRepository sampleRepository;
--- src/main/java/ex/sample/domain/sample/implementation/SampleSaver.java
+++ src/main/java/ex/sample/domain/sample/implementation/SampleSaver.java
@@ -6,10 +6,12 @@ import ex.sample.domain.sample.infrastructure.SampleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class SampleSaver {
 
     private final SampleRepository sampleRepository;
