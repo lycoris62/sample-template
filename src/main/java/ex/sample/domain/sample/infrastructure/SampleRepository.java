36aa7f3fb9f2bcddc8b8ee47fed490ac44178fd1
yunjae62 <wodbs7758@naver.com>
2024-10-31 11:43:16 +0900

    remove: 기존 SampleRepository 삭제

--- src/main/java/ex/sample/domain/sample/infrastructure/SampleRepository.java
+++
@@ -1,9 +0,0 @@
-package ex.sample.domain.sample.infrastructure;
-
-import ex.sample.domain.sample.domain.Sample;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleRepository extends JpaRepository<Sample, UUID> {
-
-}
